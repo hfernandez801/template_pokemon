70794d5246825071e1c5a281c12e3b9df749c09b
hfernandez <hfernandez@iw.digital>
2021-02-19 21:53:22 -0600

    Prefix path

--- PokemonFilter/urls.py
+++ PokemonFilter/urls.py
@@ -4,10 +4,17 @@ from __future__ import unicode_literals
 #Standar imports
 #Related third party
 from django.conf.urls import url, include
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 #Local imports
 
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+
+    # PRODUCTION LINUX ENVIRONMENT
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    #url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 ]
